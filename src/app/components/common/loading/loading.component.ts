03020e87dd1b901fb3aa9a11fdfe470e4d3b05bd
Parin Gandhi <parin.a.gandhi@gmail.com>
2025-11-05 09:30:04 -0500

    Added loading component

---
+++ src/app/components/common/loading/loading.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ptacts-loading',
+  imports: [],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.scss'
+})
+export class LoadingComponent {
+
+}
